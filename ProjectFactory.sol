aa410fca062e51acc567dabed15f02f9e8edb3b1
Martín Stancanelli <mastanca14@gmail.com>
2019-11-01 21:44:00 -0300

    create function to audit project

--- ProjectFactory.sol
+++ ProjectFactory.sol
@@ -16,20 +16,23 @@ contract ProjectFactory is Owned {
         uint creationDate;
         uint endDate;
         States state;
+        uint amountContributed; 
         address[] contributorsAddresses;
         mapping(address => uint) contributors;
     }
 
     event NewProject(uint id, string name, uint amount);
+    event ProjectStateChanged(uint id, string name, States state);
 
     Project[] public projects;
+    mapping(uint => Project) projectsMap;
     mapping(uint => address) public projectsToOwner;
-    mapping(address => uint) ownerProjectsCount;
 
     function createProject(string _name, uint _amount, uint _days) public {
-        uint id = projects.push(Project(_name, _amount, now, uint(now + _days), States.OPEN, new address[](10))) - 1;
+        Project memory project = Project(_name, _amount, now, uint(now + _days), States.OPEN, new address[](10));
+        uint id = projects.push(project) - 1;
         projectsToOwner[id] = msg.sender;
-        ownerProjectsCount[msg.sender]++;
+        projectsMap[id] = project;
         emit NewProject(id, _name, _amount);
     }
 
@@ -37,11 +40,26 @@ contract ProjectFactory is Owned {
         Project storage project = projects[_id];
         project.contributors[msg.sender] += msg.value;
         project.contributorsAddresses.push(msg.sender);
-        project.amount += msg.value;
+        project.amountContributed += msg.value;
     }
 
     function getAmountContributedFor(uint projectId) public view returns (uint amount) {
         Project storage project = projects[projectId];
-        return project.amount;
+        return project.amountContributed;
+    }
+
+    function auditProject(uint projectId) public {
+        Project memory project = projects[projectId];
+        if (project.amountContributed >= project.amount) {
+            project.state = States.CLOSE;
+            projectsToOwner[project.id].transfer(project.amountContributed);
+        } else if (project.endDate < now()) {
+            project.state = States.CANCELED;
+            for (uint index = 0; index < project.contributorsAddresses.length; index++) {
+                address contributor = project.contributorsAddresses[index];
+                contributor.transfer(project.contributors[contributor]);
+            }
+        }
+        emit ProjectStateChanged(project.id, project.name, project.state);
     }
 }
